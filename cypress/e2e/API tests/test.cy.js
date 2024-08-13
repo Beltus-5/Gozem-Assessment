58f22a2c85c46daf964e548a8b37aacae4a6d64e
unknown <beltustahbod@gmail.com>
2024-08-13 17:33:10 +0100

    added notes and steps on README.md

--- cypress/e2e/API tests/test.cy.js
+++ cypress/e2e/API tests/test.cy.js
@@ -1,9 +1,11 @@
 /// <reference types="cypress" />
 
 
-describe('example to-do app', () => {
+describe('Gozem-Assessment', () => {// Authinticating the API//
   let api_tocken, myBooking, myBookingId = '';
+    // This block runs once before all tests in the suite
   before(() => {
+       // Sending a POST request to authenticate and get an API token
     cy.request({
       method: 'POST',
       url: "/auth",
@@ -15,14 +17,18 @@ describe('example to-do app', () => {
         "password" : "password123"
     }
     }).then((response) => {
+       // Verifying the response status is 200 (OK)
       expect(response.status).to.equal(200)
+       // Logging the response body to the Cypress console
       cy.log(response.body)
+       // Storing the API token for use in subsequent requests
       api_tocken = response.body.token
       cy.log(api_tocken)
     })
   })
-
+  // Test case for creating a booking
   it('Create Booking', () =>{
+      // Sending a POST request to create a new booking
     cy.request({
     method: 'POST',
     url: '/booking',
@@ -44,14 +50,17 @@ describe('example to-do app', () => {
     }
     
   }).then((response) => {
+      // Sending a POST request to create a new booking
     expect(response.status).to.equal(200)
     cy.log(response.body);
+     // Logging the response body to the Cypress console
     myBookingId = response.body.bookingid;
     cy.log(myBookingId);
   })
 })
-
-  it('Read Booking', () =>{
+    // Test case for reading a booking
+  it('Read Booking', () =>{ 
+      // Sending a GET request to retrieve the booking details
     cy.request({
     method: 'GET',
     url: '/booking/' + myBookingId,
@@ -60,9 +69,12 @@ describe('example to-do app', () => {
       'Accept': ' application/json'
     }
   }).then((response) => {
+      // Verifying the response status is 200 (OK)
     expect(response.status).to.equal(200)
+     // Storing the booking details for use in subsequent tests
     myBooking = response.body;
     cy.log(myBooking);
+     // Logging each key-value pair in the booking details to the Cypress console
     Object.entries(myBooking).forEach(([key, value]) => {
       if(typeof value === 'object' && value !== null){
         Object.entries(value).forEach(([key, value]) => {
@@ -75,8 +87,9 @@ describe('example to-do app', () => {
     })
     })
   })
+       // Updating the created booking above by changing the check out date and additional needs.
+  it('Update Booking', () => { 
 
-  it('Update Booking', () => {
     cy.request({
       method: 'PUT',
       url: '/booking/' + myBookingId,
@@ -98,9 +111,12 @@ describe('example to-do app', () => {
         "additionalneeds" : "Beds"
       }
     }).then((response) => {
+      // Verifying the response status is 200 (OK)
       expect(response.status).to.equal(200)
+       // Storing the updated booking details
       myBooking = response.body;
       cy.log(myBooking);
+       // Logging each key-value pair in the updated booking details to the Cypress console
       Object.entries(myBooking).forEach(([key, value]) => {
         if(typeof value === 'object' && value !== null){
           Object.entries(value).forEach(([key, value]) => {
@@ -113,7 +129,7 @@ describe('example to-do app', () => {
       })
       })
   })
-
+    //  Deleting the created booking above 
   it('Update Booking', () => {
     cy.request({
       method: 'DELETE',
@@ -125,6 +141,7 @@ describe('example to-do app', () => {
         'Authorization': 'Basic YWRtaW46cGFzc3dvcmQxMjM='
       },
     }).then((response) => {
+        // Verifying the response status is 201 (Created)
       expect(response.status).to.equal(201)
 
       })
